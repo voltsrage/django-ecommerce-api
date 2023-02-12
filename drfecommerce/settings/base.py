4d616410ee4798d40b314703bfb0db8393ce7d66
voltsrage <electric5388@hotmail.com>
2023-02-12 15:43:23 +0800

    Add formatting and liniting

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
